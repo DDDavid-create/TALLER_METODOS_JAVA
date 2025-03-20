a5d5f33aa5c72a99af5125e9f95616f71da61163
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-03-19 20:38:03 -0500

    EJERCICIO_2 COMPLETE

--- src/Ejercicio_2/Principal.java
+++ src/Ejercicio_2/Principal.java
@@ -1,7 +1,7 @@
 package Ejercicio_2;
 
 public class Principal {
-    /*TODO Escribe un método con parámetros y sin retorno que reciba un número entero y lo imprima en pantalla si es mayor que 10.*/
+    /*Escribe un métod con parámetros y sin retorno que reciba un número entero y lo imprima en pantalla si es mayor que 10.*/
     public static void main(String[] arg){
         new Metodos();
     }

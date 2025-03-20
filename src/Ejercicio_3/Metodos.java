40aa525fce128c0b3471bf187e0ce15249d9e2be
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-03-19 20:29:31 -0500

    EJERCICIO_3 COMPLETE

--- src/Ejercicio_3/Metodos.java
+++ src/Ejercicio_3/Metodos.java
@@ -1,4 +1,21 @@
 package Ejercicio_3;
 
+import javax.swing.*;
+
 public class Metodos {
+
+    public Metodos(){
+        int num = generarNumAle();
+        String msj = "Generador de numeros aleatorios entre 1-100 \n\n";
+        msj+= "Tu numero generado es: " + num;
+
+        JOptionPane.showMessageDialog(null, msj);
+    }
+
+    public int generarNumAle(){
+        // Math.random() genera un número decimal aleatorio entre 0.0 y 1.0 (excluyendo 1.0)
+        // Multiplicamos por 100 para obtener un rango entre 0 y 100
+        // Sumamos 1 para que el rango sea de 1 a 100 en lugar de 0 a 99
+        return (int) (Math.random()*100) + 1;
+    }
 }
--- src/Ejercicio_3/Principal.java
+++ src/Ejercicio_3/Principal.java
@@ -1,4 +1,9 @@
 package Ejercicio_3;
 
 public class Principal {
+    /*Escribe un métod sin parámetros y con retorno que genere un número aleatorio entre 1 y 100, lo devuelva como resultado e imprima el numero retornado. (Investigar sobre Math random)*/
+
+    public static void main(String[] arg){
+        new Metodos();
+    }
 }
